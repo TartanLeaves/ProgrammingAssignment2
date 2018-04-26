df20ed60a8b336480bde1fafa431717c203ea347
TartanLeaves <38750406+TartanLeaves@users.noreply.github.com>
2018-04-26 12:02:11 +0200

    Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
+###############################################################################
+###                                                                         ###
+###    Peer-graded Assignment: Programming Assignment 2: Lexical Scoping    ###
+###                                                                         ###
+###############################################################################
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+## 1) A Function that takes a sqaure invertible matrix as argument and that:
+        # - returns the matrix itself via get.x
+        # - calculates the inverse of the matrix (without output) via calc.inv
+        # - returns the inverse of the matrix, if it has been calculated, via get.inv
+
+    makeCacheMatrix <- function(x = matrix()) {
+        inv<-NULL
+        
+        get.x <- function() x
+        calc.inv <- function() inv<<-solve(x)
+        get.inv <- function() inv
+        
+        list(
+            get.x = get.x,
+            calc.inv = calc.inv,
+            get.inv = get.inv
+            )
+    }
+
+
+## 2) A Function that calculates the inverse of a matrix, if it has not been stored in cache before
+
+    cacheSolve <- function(x, ...) {
+        
+        # Get the inverse of the function from before:
+        # (It is NULL if it has not been calculated)
+        inv <- x$get.inv()
+        
+        if (!is.null(inv)) {
+            message("getting cached data")
+            return(Matrix$get.inv())
+        }
+        
+        mat<-Matrix$get.x()
+        inv<-solve(mat, ...)
+        inv
+        
+    }
